caee57da787a8c31f23e989510fc23c2c82857a3
Crowdbotics <team@crowdbotics.com>
2020-10-19 21:50:47 +0000

    Update models

--- backend/denscity_21728/wsgi.py
+++ backend/denscity_21728/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'denscity_21728.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "denscity_21728.settings")
 
 application = get_wsgi_application()
